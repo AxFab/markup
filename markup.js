96ead6eea172300c38c4fd91a13b742b207559b1
Fabien Bavent <fabien.bavent@gmail.com>
2014-10-28 22:02:35 +0100

    [+] First standalone version of markup

---
+++ markup.js
@@ -0,0 +1,319 @@
+/*!
+*   Axml
+* Copyright 2014-2015 Fabien Bavent
+* Released under the BSD license
+*/
+(function () {
+  var previous_mod, root = this
+  if (root != null)
+    previous_mod = root.markup
+  
+  var DEBUG = true
+
+// Dependancies ===========================================================
+  var fs = root.fs
+  if( typeof fs === 'undefined' ) {
+    if( typeof require !== 'undefined' ) {
+      fs = require('fs')
+    }
+    else throw new Error('markup requires fs');
+  }
+
+  var async = root.async
+  if( typeof async === 'undefined' ) {
+    if( typeof require !== 'undefined' ) {
+      async = require('async')
+    }
+    else throw new Error('markup requires async');
+  }
+
+  var markupCtx = root.markupCtx
+  if( typeof markupCtx === 'undefined' ) {
+    if( typeof require !== 'undefined' ) {
+      markupCtx = require('./markupCtx')
+    }
+    else throw new Error('markup requires markupCtx');
+  }
+
+// Module markup =========================================================
+
+  var markup = function (options) {
+    if (!(this instanceof markup))
+      return new markup(options);
+
+    this.super_ = stream.Transform
+    stream.Transform.call (this)
+    this._init (options)
+  }
+
+  // ======================================================================
+  // axml.prototype = Object.create (stream.Transform.prototype, {})
+  markup.commands = {
+
+    extends:function (mark, ctx) {
+      if (mark.value.length != 1) {
+        console.error ('The command \'extends\' take one argument');
+        return null;
+      }
+      ctx.setParent (ctx.eval (mark.value[0]))
+      return null;
+    },
+
+    set:function (mark, ctx) {
+      if (mark.value.length != 2) {
+        console.error ('The command \'set\' take two arguments');
+        return null;
+      }
+      ctx.varSet(mark.value[0], ctx.eval (mark.value[1]))
+      return null
+    },
+
+    get:function (mark, ctx) {
+      if (mark.value.length != 1) {
+        console.error ('The command \'get\' take one argument');
+        return null;
+      }
+      return { type:'T', value:ctx.eval (mark.value[0])}
+    },
+
+    get_if:function (mark, ctx) {
+      if (mark.value.length != 3) {
+        console.error ('The command \'get_if\' take three arguments');
+        return null;
+      }
+      
+      var cdt = ctx.eval (mark.value[0]);
+      var v = (cdt != '' && cdt != false) ? 1 : 2
+      return { type:'T', value:ctx.eval (mark.value[v]) }
+    },
+
+    doLayout:function (mark, ctx, callback) {
+      if (mark.value.length != 1) {
+        callback ('The command \'doLayout\' take one argument');
+        return null;
+      } else {
+        mark.data = ctx.childData
+        callback();
+      }
+    },
+
+    include:function (mark, ctx, callback) {
+      if (mark.value.length != 1) {
+        callback ('The command \'include\' take one argument');
+        return null;
+      } else {
+        markup.renderFile (ctx.eval(mark.value[0]), ctx, function (err, data) {
+
+          mark.data = data
+          callback();
+        })
+      }
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  // axml.prototype._transform = function(chunk, encoding, callback) {
+  markup.directory = './views'
+
+  // ----------------------------------------------------------------------
+  markup.renderFile = function (path, options, callback) {
+
+    if (path.startswith ('/'))
+      path = markup.directory + path;
+
+    if (options == null)
+      options = new markupCtx({});
+    else if (!options.getParent)
+      options = new markupCtx(options)
+
+    // if (DEBUG) console.log ('renderFile', path)
+    fs.readFile (path, function (err, data) {
+      if (err && !path.endswith ('/500.html')) {
+        console.error (path, err)
+        markup.renderFile ('/500.html', options, callback)
+      } else if (err) {
+        callback (err, null)
+      } else {
+        // Note: sending a buffer will put the file to download, send a string!
+        markup.parseFile (data, options, callback)
+      }
+
+    })
+    // callback (null, '<h1>Yep !?</h1>\n')
+  }
+
+  // ----------------------------------------------------------------------
+  markup.finish = function (array, options, callback) {
+
+    data = ''
+    for (var i=0; i < array.length; ++i) {
+      if (typeof array[i] === 'string')
+        data += array[i]
+      else if (array[i].data != null)
+        data += array[i].data
+    }
+
+    if (options.getParent() == null) {
+      callback (null, data)
+    } else {
+      options.childData = data
+      var url = options.getParent()
+      options.setParent(null)
+      markup.renderFile (url, options, callback)
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  markup.doCommand = function (cmd, options, callback) {
+
+    // if (cmd == 'extends')
+    markup.commands[cmd.type] (cmd, options, callback)
+      // cmd.data = cmd.type
+      // callback ()
+  }
+
+  // ----------------------------------------------------------------------
+  markup.parseFile = function (data, options, callback) {
+    // console.log (data.toString())
+    var array = []
+    var parent = null;
+    data = data.toString()
+    var commands = []
+
+    // Split data and commands
+    for (;;) {
+      var l = data.match(/#\{.*\/\}/)
+      if (l == null) break
+      if (l.index > 0)
+        array.push (data.substring(0, l.index))
+      data = data.substring (l.index + l[0].length)
+      var cmd = markup.parseCommand(l[0])
+      array.push (cmd)
+      if (cmd.type != 'extends')
+        commands.push (cmd)
+      else
+        parent = cmd 
+    }
+
+    array.push (data)
+
+    // Regroup page
+    if (commands.length == 0) {
+      if (parent)
+        markup.commands.extends (parent, options)
+      markup.finish (array, options, callback)
+
+    } else {
+
+      async.each (commands, function (cmd, callback) {
+        markup.commands[cmd.type] (cmd, options, callback)
+      }, function (err) {
+        if (err) callback (err, null)
+        else {
+              
+          if (parent)
+            markup.commands.extends (parent, options)
+          markup.finish (array, options, callback)
+        }
+      })
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  markup.parseCommand = function (mark) {
+
+    var tag = mark.substring(2);
+    var c = '';
+    
+    while (tag[0] <= ' ')
+      tag = tag.substring(1);
+
+    while (tag[0] > ' ') {
+      c += tag.substring(0, 1);
+      tag = tag.substring(1);
+    }
+    
+    if (c == '/}') {
+      console.warn('Empty tag')
+    }
+    
+    var command = c;
+    var args = [];
+    c = '';
+    for (;;) {
+      while (tag[0] <= ' ')
+        tag = tag.substring(1);
+
+      while (tag[0] != ':' && (tag[0] != '/' || tag[1] != '}')) { 
+        c += tag.substring(0, 1);
+        tag = tag.substring(1);
+      }
+      
+      if (c == '/}' || c == '}') {
+        console.log('Cmd: '+ args);
+        return;
+      }
+      
+      c = c.trim();
+      args.push(c);
+      c = '';
+      
+      if (tag[0] == '/' && tag[1] == '}')
+        return {type:command, value:args };
+      tag = tag.substring(1);
+    }
+  }
+
+  // ----------------------------------------------------------------------
+  markup.lookFor = function(req, res) {
+    if (req.url == '/')
+      res.render ('index');
+    else {
+      // Note: As express doesn't allow to recover we try to check 
+      // everything first, but it's a bad design !
+      fs.exists (markup.directory + req.url, function (exist) {
+        var name = req.url.substring(1, req.url.length - 5) // Seriously !?
+        if (exist) 
+          res.render (name);
+        else 
+          res.render ('404'); // File not found!
+      })
+    }
+  }
+
+
+
+
+// Export the module ======================================================
+  markup.noConflict = function () {
+    root.markup = previous_mod
+    return markup
+  }
+
+  if (typeof module !== 'undefined' && module.exports) // Node.js
+    module.exports = markup
+  else if (typeof exports !== 'undefined')
+    exports = module.exports = markup
+  else if (typeof define !== 'undefined' && define.amd) // AMD / RequireJS
+    define([], function () { return markup })
+  else // Browser
+    root.markup = markup
+  
+
+  if (require && require.main === module) {
+
+    // console.log (process.argv[2])
+    markup.renderFile(process.argv[2], null, function (err, data) {
+      console.log ('DATA:')
+      console.log (data)
+    })
+  }
+
+
+}).call(this)
+
+
+
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
---
+++ markupCtx.js
@@ -0,0 +1,228 @@
+/*!
+*   Axml
+* Copyright 2014-2015 Fabien Bavent
+* Released under the BSD license
+*/
+(function () {
+  var previous_mod, root = this
+  if (root != null)
+    previous_mod = root.markupCtx
+  
+// Dependancies ===========================================================
+  // var fs = root.fs
+  // if( typeof fs === 'undefined' ) {
+  //   if( typeof require !== 'undefined' ) {
+  //     fs = require('fs')
+  //   }
+  //   else throw new Error('markupCtx requires fs');
+  // }
+
+// String extentions ======================================================
+
+  String.prototype.trim=function() {
+    return this.replace(/^\s+|\s+$/g, '');
+  }
+
+  // ----------------------------------------------------------------------
+  String.prototype.ltrim=function() {
+    return this.replace(/^\s+/,'');
+  }
+
+  // ----------------------------------------------------------------------
+  String.prototype.rtrim=function() {
+    return this.replace(/\s+$/,'');
+  }
+
+  // ----------------------------------------------------------------------
+  String.prototype.fulltrim=function() {
+    return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' ');
+  }
+
+  // ----------------------------------------------------------------------
+  String.prototype.startswith=function(str) {
+    return this.substring(0, str.length) == str;
+  }
+
+  // ----------------------------------------------------------------------
+  String.prototype.endswith=function(str) {
+    return this.substring(this.length - str.length, this.length) == str;
+  }
+
+
+// Module markupCtx ==========================================================
+  function markupCtx (req) {
+
+    // object scope
+    this.current = {};
+    this.query = {};
+    this.session = {};
+    this.il8n = {};
+    this.system = {};
+    
+    // caching capability
+    this.caching = 'static';
+    this.sessionUpdate = false;
+    
+    /**
+     * Get the value of a variable
+     * @param {String} [name] variable name
+     * @return {String} Value store on the variable
+     * @todo Add warning variable is undefined!
+     */   
+    this.varGet = function (name) {
+      if (name.startswith('query.')) {
+        if (this.caching == 'static')
+          this.caching = 'query';
+        return this.query[name.substring(6, name.length)];
+      }
+      if (name.startswith('session.')) {
+        if (this.caching != 'volatile')
+          this.caching = 'session';
+        return this.session[name.substring(8, name.length)];
+      }
+      if (name.startswith('il8n.')) 
+        return this.il8n[name.substring(5, name.length)];
+      if (name.startswith('system.')) {
+        this.caching = 'volatile';
+        return this.system[name.substring(7, name.length)];
+      }
+      if (name.startswith('current.')) 
+        return this.current[name.substring(8, name.length)];
+      return this.current[name];
+    };
+    
+    /**
+     * Assign a variable
+     * @param {String} [name] variable name
+     * @param {String} [value] value to store on variable
+     */   
+    this.varSet = function (name, value) {
+      if (name.startswith('session.')) {
+        this.sessionUpdate = true;
+        this.session[name.substring(8, name.length)] = value;
+      }
+      if (name.startswith('current.')) 
+        this.current[name.substring(8, name.length)] = value;
+      if (name.startswith('system.') ||
+        name.startswith('query.') ||
+        name.startswith('il8n.'))
+        throw new Error("Variable " + name + " is read only");
+      return this.current[name] = value;
+    };
+    
+    this.setParent = function (uri) {
+      this.parentUrl = uri;
+    }
+
+    this.getParent = function () {
+      return this.parentUrl;
+    }
+
+    function dummyTextEval (value) 
+    {
+      var tokens = []
+      var str = '';
+      var stre = '';
+      var lg = 0
+      while (lg < value.length) {
+        if (value[lg] == '\'') {
+          while (value[++lg] != '\'' && lg < value.length) {
+            if (value[lg] == '\\')
+              ++lg;
+            str += value[lg]
+          }
+          tokens.push (str);
+          str = '';
+        } else if (value[lg] == '"') {
+          while (value[++lg] != '"' && lg < value.length) {
+            if (value[lg] == '\\')
+              ++lg;
+            str += value[lg]
+          }
+          tokens.push (str);
+          str = '';
+        } else {
+          while (value[lg] != ' ' && lg < value.length)
+            str += value[lg++]
+          if (str != '+' && str != '-z' && str != '-n' && str != '-eq' && str != '-ne' && str != '=' && str != '!=') // TODO contains in list of operator !!
+            stre = that.varGet (str)
+          else stre = str
+          if (stre == null) {
+            console.warn ('the variable \'' + str + '\' is undefined');
+          }
+          tokens.push (stre != null ? stre : '');
+          str = '';
+        }
+        
+        lg++;
+      }
+      // console.log ('token', tokens.toString())
+      
+      if (tokens.length == 1)
+        return tokens[0];
+      else {
+        str = ''
+        for  (var i =0; i<tokens.length; ++i) {
+          switch(tokens[i]) {
+            case '+':
+              if (i+1 < tokens.length)
+                str += tokens[++i];
+              break;
+            case '-z':
+              if (i+1 < tokens.length)
+                str = (tokens[++i] == '' ? true : false);
+              break;
+            case '-n':
+              if (i+1 < tokens.length)
+                str = (tokens[++i] != '' ? true : false);
+              break;
+            case '=':
+            case '-eq':
+              if (i+1 < tokens.length)
+                str = (tokens[++i] == str ? true : false);
+              break;
+            case '!=':
+            case '-ne':
+              if (i+1 < tokens.length)
+                str = (tokens[++i] != str ? true : false);
+              break;
+            default:
+              str += tokens[i];
+              break;
+          }
+        }
+       return str;
+      }
+    }
+    
+    this.eval = function (string) {
+      return dummyTextEval (string);
+    }
+    
+    var that = this;
+    {
+      this.query = req.query
+    }
+  }
+
+
+
+// Export the module ======================================================
+  markupCtx.noConflict = function () {
+    root.markupCtx = previous_mod
+    return markupCtx
+  }
+
+  if (typeof module !== 'undefined' && module.exports) // Node.js
+    module.exports = markupCtx
+  else if (typeof exports !== 'undefined')
+    exports = module.exports = markupCtx
+  else if (typeof define !== 'undefined' && define.amd) // AMD / RequireJS
+    define([], function () { return markupCtx })
+  else // Browser
+    root.markupCtx = markupCtx
+  
+}).call(this)
+
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
